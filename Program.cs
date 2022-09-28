88b2a6890e1405b8dfe4bfba97e4cee489a24248
Daniella <103566287+tryklick@users.noreply.github.com>
2022-09-28 17:14:48 +0200

    Miniräknare

--- Program.cs
+++ Program.cs
@@ -1,185 +1,141 @@
-﻿using System;
+﻿Console.ForegroundColor = ConsoleColor.Yellow;
 
-namespace Miniräknare
+int menyVal = 0;
+int minTal = 1;
+int maxTal = 13;
+int dittTal = 0;
+int datornsTal = 0;
+string svårGrad = " ";
+string välkomstMeddelande = "Hej, välkommen till Gissa Avfarten.";
+
+string speletsRegler = "I varje omgång av Gissa Avfarten ska datorn slumpa fram ett heltal, som representerar en avfart, mellan 1 och 13." +
+    "\nAnvändaren ska gissa vilket talet är ända tills hen gissar rätt. " +
+    "\nEfter varje gissning ska användaren få veta om gissningen var rätt, för hög eller för låg.";
+
+Random slumpTal = new Random();
+
+// Välkomna användaren till Gissa Talet
+for (int i = 0; i < välkomstMeddelande.Length; i++)
 {
-    class Program
+    Console.Write(välkomstMeddelande[i]);
+    if (välkomstMeddelande[i] == '.')
     {
-        static void Main(string[] args)
+        Thread.Sleep(1000);
+    }
+    else
+    {
+        Thread.Sleep(100);
+    }
+
+}
+Console.WriteLine();
+Console.WriteLine("Gör ett menyval.");
+
+//Presentera menyval
+while (menyVal != 4)
+{
+    Console.WriteLine("1. Spela Gissa Avfarten.");
+    Console.WriteLine("2. Spelets regler.");
+    Console.WriteLine("3. Svårighetsgrad.");
+    Console.WriteLine("4. Avsluta program.");
+
+    menyVal = Convert.ToInt32(Console.ReadLine());
+
+    switch (menyVal)
+    {
+
+        case 1:
+            //Datorn slumpar fram ett tal mellan 1-10
+            datornsTal = slumpTal.Next(minTal, 10);
+            MetodEtt(minTal, maxTal);
+                
+
+            break;
+
+        case 2:
+
+            Console.WriteLine($"Detta är spelets regler: {speletsRegler}");
+
+            break;
+
+        case 3:
+
+            Console.WriteLine("Välj svårighetsgrad. Skriv lätt eller svår.");
+            Console.WriteLine("Lätt: Du gissar på samma slumpade tal.");
+            Console.WriteLine("Modulär: Du gissar på nytt slumpat tal.");
+            svårGrad = Console.ReadLine();
+
+            if (svårGrad.ToLower() == "lätt")
+            {
+                MetodEtt(minTal, maxTal);
+
+                break;
+
+            }
+            else if (svårGrad.ToLower() == "modulär")
+            {
+                Console.WriteLine("Välj minsta möjliga tal");
+                int.TryParse(Console.ReadLine(), out minTal);
+
+                Console.WriteLine(" Välj största möjliga tal");
+                int.TryParse(Console.ReadLine(), out maxTal);
+
+                MetodEtt(minTal, maxTal);
+                break;
+            }
+
+
+            break;
+
+        case 4:
+
+            Console.WriteLine("Programmet avslutas..");
+
+            break;
+
+        default:
+
+            Console.WriteLine("     /\\\n    /  \\\n   /    \\\n  /   !  \\\n /________\\");
+            Console.WriteLine("Felaktig inmatning.. vänligen försök igen. Välj menyval 1-4.");
+
+            break;
+    }
+
+
+}
+Console.ReadLine();
+
+ void MetodEtt(int min, int max)
+{
+    datornsTal= slumpTal.Next(min, max);
+
+    //Användaren gissar vilket tal som datorn slumpat fram
+    Console.WriteLine($"Gissa vilken avfart som leder till Arlanda,{min} - {max}");
+    int.TryParse(Console.ReadLine(), out dittTal);
+    //dittTal = Convert.ToInt32(Console.ReadLine());
+
+    //Användaren gissar tills talet är rätt
+    //Efter varje gissning får användaren veta om hen gissat rätt, för högt eller för lågt.
+    while (dittTal != datornsTal)
+    {
+        Console.Clear();
+        if (dittTal > datornsTal)
+        {
+            Console.WriteLine("     /\\\n    /  \\\n   /    \\\n  /   !  \\\n /________\\");
+            Console.WriteLine($"Du gissade fel. Avfartens nummer {dittTal} var för högt.");
+        }
+
+        else if (dittTal < datornsTal)
 
         {
-            Console.WriteLine("Välkommen till miniräknaren!");
-
-            Console.Write("\n __*____/____+______-_____\n");
-
-            Console.Write("\nHallå där!\n");
-
-            // Välkomnande meddelande
-
-
-
-
-            List<string> ResultatHistoria = new List<string>();
-
-            // En lista för att spara historik för räkningar
-
-
-            while (true)
-            {
-                string Operator = "";
-                string total = "";
-                string input1 = "";
-                string input2 = "";
-                double Resultat = 0;
-                double tal1 = 0;
-                double tal2 = 0;
-
-                
-
-                Console.WriteLine("Uppge ditt första heltal");
-                input1 = Console.ReadLine();
-                while (!double.TryParse(input1, out tal1))
-                {
-                    if (input1 == " ")
-                    {
-                        Console.WriteLine("Skriv in ett tal:");
-                        input1 = Console.ReadLine();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Inaktuell,vad vänligt skriv in ett heltal!");
-                        input1 = Console.ReadLine();
-                    }
-                }
-                // Användaren matar in tal och matematiska operation
-                //OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
-
-
-
-                Console.WriteLine("Välje en operator ( -, +, *, / )");
-                Operator = Console.ReadLine();
-
-                while (true)
-                {
-
-                    if (Operator == "*" || Operator == "+" || Operator == "-" || Operator == "/")
-                    {
-                        break;
-                    }
-                    else if (Operator == "EXIT")
-                    {
-
-                        Console.WriteLine("Hej");
-                        Environment.Exit(10);
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("operator");
-                        Operator = Console.ReadLine();
-                    }
-
-                } // Användaren matar in tal och matematiska operation
-
-
-
-
-                Console.WriteLine("Uppge andra talet");
-                input2 = Console.ReadLine();
-                while (!double.TryParse(input2, out tal2))
-                {
-                    if (input2 == " ")
-                    {
-                        Console.WriteLine("Skriv in ett tal::");
-                        input2 = Console.ReadLine();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Inaktuell,välj ett heltal ");
-                        input2 = Console.ReadLine();
-                    }
-                }
-
-                
-                switch (Operator)
-                {
-                    case "/":
-                        Resultat = tal1 / tal2;
-                        break;
-                    case "*":
-                        Resultat = tal1 * tal2;
-                        break;
-                    case "-":
-                        Resultat = tal1 - tal2;
-                        break;
-                    case "+":
-                        Resultat = tal1 + tal2;
-                        break;
-
-                }
-
-                
-                if (Operator == "-")
-                {
-                    total = tal1 + " - " + tal2 + " = " + Resultat.ToString();
-                    Console.WriteLine(total);
-                }
-                else if (Operator == "+")
-                {
-                    total = tal1 + " + " + tal2 + " = " + Resultat.ToString();
-                    Console.WriteLine(total);
-                }
-                else if (Operator == "*")
-                {
-                    total = tal1 + " * " + tal2 + " = " + Resultat.ToString();
-                    Console.WriteLine(total);
-                }
-                else if (Operator == "/")
-                {
-                    
-                    if (tal2 == 0)
-                    {
-                        Console.WriteLine("\n Inaktuell, vad vänligt välj en av operator.");
-                    }
-
-                    else
-                    {
-                        total = tal1 + " / " + tal2 + " = " + Resultat.ToString();
-                        Console.WriteLine(total);
-                    }
-
-
-                    // Användaren matar in tal och matematiska operation
-                    //OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
-                    // Ifall användaren skulle dela 0 med 0 visa Ogiltig inmatning!
-
-
-
-
-                }
-                //resusltat till listan 
-                ResultatHistoria.Add(total);
-
-                
-                Console.Write("\n __*____/____+______-_____\n" +
-                    "Om du vill se tidigare resultat klicka på ( h ) eller på ( ENTER ) för att fortsätta: ");
-                if (Console.ReadLine() == "h")
-                {
-                    foreach (string h in ResultatHistoria)
-                    {
-                        Console.WriteLine("\n" + h);
-                    }
-                }
-
-                
-                Console.Write("Om du vill avsluta pårgramet tryck ( a ) eller på ( ENTER ) för en ny beräkning: ");
-                if (Console.ReadLine() == "a")
-                {
-                    break;
-                }
-                Console.Write("\n __*____/____+______-_____\n");
-            }
-            //Fråga användaren om den vill avsluta eller fortsätta.
-
+            Console.WriteLine("     /\\\n    /  \\\n   /    \\\n  /   !  \\\n /________\\");
+            Console.WriteLine($"Du gissade fel. Avfartens nummer {dittTal} var för lågt.");
         }
+
+        Console.WriteLine($"Gissa talet igen, {min} - {max}");
+        dittTal = Convert.ToInt32(Console.ReadLine());
+
     }
+    Console.Clear();
+    Console.WriteLine($"Grattis! Du gissade rätt. Den korrekta avfarten till Arlanda var {dittTal}!");
 }
